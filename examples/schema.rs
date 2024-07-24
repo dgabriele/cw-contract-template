2bc6c7a6dda7ffea9d24a529496bfa737fe70d15
Daniel Gabriele <dgabriele@users.noreply.github.com>
2024-07-24 11:02:47 -0400

    Update schema.rs

--- examples/schema.rs
+++ examples/schema.rs
@@ -4,7 +4,7 @@ use std::fs::create_dir_all;
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 use cosmwasm_std::Coin;
 
-use cw_contract::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use contract::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
   let mut out_dir = current_dir().unwrap();
