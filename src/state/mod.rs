6e8c3795ccdacd82de949dc639f59ce0d5c637eb
Daniel Gabriele <dgabriele@users.noreply.github.com>
2024-07-24 11:03:44 -0400

    Update mod.rs

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -8,7 +8,7 @@ use crate::{error::ContractError, execute::Context, msg::InstantiateMsg};
 /// Top-level initialization of contract state
 pub fn init(
     _ctx: Context,
-    _msg: &InstantiateMsg,
+    _msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
     Ok(Response::new().add_attribute("action", "instantiate"))
 }
